331baed2cd4ca8c272bee4f5a5b6de055a483f5b
Kush Patel <kushrpatel06@yahoo.com>
2022-10-12 16:02:04 -0400

    Bug Fixes, Local storage functionality and finished Styling Added Feature: Users can click on buttons on the left to search through their previous search history Fixed serval bugs involving local storage not rendering to the page correctly, local storage not saving corrently, styling not being applied correctly, etc etc. Finished styling

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,6 @@
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+const history = document.querySelector("#history");
+
 let weather ={
     apiKey: "fe4fff594a4888855d1b1cb52405061f",
     fetchWeather: function (city) {
@@ -14,7 +17,6 @@ let weather ={
         const humidity = data.list[0].main.humidity;
         const windSpeed = data.list[0].wind.speed;
         const icon = data.list[0].weather[0].icon;
-        console.log(cityName, temperature, humidity, windSpeed, icon);
         //replacing the default text with search results info
         document.querySelector("#cityName").innerText = cityName;
         document.querySelector("#temperature").innerText = temperature + "°C";
@@ -23,18 +25,9 @@ let weather ={
         document.querySelector("#windSpeed").innerHTML = "Wind Speed: " + windSpeed + "km/h";
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ cityName + "')";
 
-        /*
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-
-        today = mm + '/' + dd + '/' + yyyy;
-        console.log(today);
-        */
 
         var today = new Date(data.list[0].dt*1000);
-        console.log(today);
+        //console.log(today);
         var day = today.getDate();
         var month = today.getMonth() + 1;
         var year = today.getFullYear();
@@ -42,7 +35,6 @@ let weather ={
         document.querySelector("#date").innerHTML = "(" + month + "/" + day + "/" + year + ")";
 
         const fiveDayForecast = document.querySelectorAll(".five-day-forecast");
-        console.log(fiveDayForecast.length);
         for(var c = 0; c < fiveDayForecast.length; c++){
             var fiveDayForecastDate = new Date(data.list[c * 8 + 4].dt * 1000);
             /*
@@ -53,8 +45,6 @@ let weather ={
             (multiplying the index by 8 gives data of 4am every day, therefore adding 4 to that value
                 yeilds the data for a much more desirably 8am)
             */
-            console.log("Five day Forecast Date Test: day " + (c + 1));
-            console.log(fiveDayForecastDate);
             //Date of this index
             var fiveDayForecastDay = fiveDayForecastDate.getDate();
             var fiveDayForecastMonth = fiveDayForecastDate.getMonth() + 1;
@@ -83,12 +73,29 @@ let weather ={
 
         }
     },
-    search: function (){
-        this.fetchWeather(document.querySelector("#searchCity").value);
+    search: function (city){
+        this.fetchWeather(city);
+        searchHistory.push(city);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+        loadHistory();
     }
 };
 
 document.querySelector("#searchBtn").addEventListener("click", function() {
+    clearCurrentForecast();
+    clearFiveDayForecast();
+    weather.search(document.querySelector("#searchCity").value);//search with button click
+});
+
+document.querySelector("#searchCity").addEventListener("keyup", function(event) {
+    if(event.key== "Enter"){
+        clearCurrentForecast();
+        clearFiveDayForecast();
+        weather.search(document.querySelector("#searchCity").value);//search with button click
+    }
+});
+
+function clearFiveDayForecast(){
     /*
     The following is made to fix a bug where the forecast card's dont clear on every search but rather
     keeps appending underneath the older searches in the same session. This is ofcourse not desirable.
@@ -108,17 +115,40 @@ document.querySelector("#searchBtn").addEventListener("click", function() {
     e.appendChild(forecastCards.cloneNode(true));
     e.appendChild(forecastCards.cloneNode(true));
     e.appendChild(forecastCards.cloneNode(true));
+}
 
-    
-    
-    weather.search();//search with button click
-});
+function clearCurrentForecast(){
+    document.querySelector("#cityName").innerText = "";
+    document.querySelector("#temperature").innerText = "°C";
+    document.querySelector("#currentIcon").src = "";
+    document.querySelector("#humidity").innerHTML = "%";
+    document.querySelector("#windSpeed").innerHTML = "km/h";
+    document.querySelector("#date").innerHTML = "";
+}
 
-document.querySelector("#searchCity").addEventListener("keyup", function(event) {
-    if(event.key== "Enter"){
-        weather.search();//search with button click
+function loadHistory(){
+    history.innerHTML = "";
+    searchHistory = [...new Set(searchHistory)];
+    for(var c = 0; c < searchHistory.length; c++){
+        console.log(searchHistory[c] + " " + c);
+        let searched = document.createElement("input");
+        searched.setAttribute("type", "text");
+        searched.setAttribute("readonly", true);
+        searched.setAttribute("class", "searched-item");
+        searched.setAttribute("value", searchHistory[c]);
+        searched.addEventListener("click", function(){
+            clearCurrentForecast();
+            clearFiveDayForecast();
+            document.querySelector("#searchCity").value = searched.value;
+            weather.search(searched.value);
+            console.log(searched.value);
+        });
+        history.append(searched);
     }
-});
+    
+}
+
 
 //Default load Toronto's weather info
 weather.fetchWeather("Toronto");
+//localStorage.clear();
